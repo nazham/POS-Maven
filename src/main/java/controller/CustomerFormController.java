210bbb46730a644f3470de63632cc74a143271ef
nazham <nazhammohamed@gmail.com>
2023-11-22 13:04:36 +0530

    Initial commit

---
+++ src/main/java/controller/CustomerFormController.java
@@ -0,0 +1,64 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class CustomerFormController {
+
+    @FXML
+    private TableColumn colAddress;
+
+    @FXML
+    private TableColumn colId;
+
+    @FXML
+    private TableColumn colName;
+
+    @FXML
+    private TableColumn colOption;
+
+    @FXML
+    private TableColumn colSalary;
+
+    @FXML
+    private TableView tblCustomer;
+
+    @FXML
+    private TextField txtAddress;
+
+    @FXML
+    private TextField txtId;
+
+    @FXML
+    private TextField txtName;
+
+    @FXML
+    private TextField txtSalary;
+
+    public void initialize(){
+
+    }
+
+    @FXML
+    void reloadButtonOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void saveButtonOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void updateButtonOnAction(ActionEvent event) {
+
+    }
+
+}
