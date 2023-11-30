ed76f91b73dd229481ac92021b68b816cbc8b544
nazham <nazhammohamed@gmail.com>
2023-12-01 00:07:32 +0530

    Place Order - Item loading to combo boxes

--- src/main/java/controller/PlaceOrderFormController.java
+++ src/main/java/controller/PlaceOrderFormController.java
@@ -3,6 +3,10 @@ package controller;
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXComboBox;
 import com.jfoenix.controls.JFXTextField;
+import dto.CustomerDto;
+import dto.ItemDto;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -11,25 +15,27 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TreeTableColumn;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+import model.CustomerModel;
+import model.ItemModel;
+import model.impl.CustomerModelImpl;
+import model.impl.ItemModelImpl;
 
 import java.io.IOException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 public class PlaceOrderFormController {
 
 
+    public JFXComboBox cmbCode;
+    public JFXComboBox cmbId;
     @FXML
     private AnchorPane pane;
     @FXML
     private Label lblTotal;
-    @FXML
-    private JFXComboBox cmbId;
-    @FXML
-    private JFXComboBox<?> cmbID;
-
-    @FXML
-    private JFXComboBox<?> cmbCode;
-
+    
     @FXML
     private JFXTextField txtName;
 
@@ -50,6 +56,64 @@ public class PlaceOrderFormController {
 
     @FXML
     private JFXButton btnPlaceOrder;
+
+    private List<CustomerDto> customers = new ArrayList<>();
+    private List<ItemDto> items = new ArrayList<>();
+
+    private CustomerModel customerModel = new CustomerModelImpl();
+    private ItemModel itemModel = new ItemModelImpl();
+
+    public void initialize(){
+        loadCustomerIds();
+        loadItemCodes();
+
+        cmbId.getSelectionModel().selectedItemProperty().addListener((observableValue, oldValue, id) -> {
+            for (CustomerDto dto:customers) {
+                if (dto.getId().equals(id)){
+                    txtName.setText(dto.getName());
+                }
+            }
+        });
+        cmbCode.getSelectionModel().selectedItemProperty().addListener((observableValue, oldValue, code) -> {
+            for (ItemDto dto:items) {
+                if (dto.getCode().equals(code)){
+                    txtDesc.setText(dto.getDesc());
+                    txtUnitPrice.setText(String.format("%.2f",dto.getUnitPrice()));
+
+                }
+            }
+        });
+    }
+
+    private void loadCustomerIds() {
+        try {
+            customers = customerModel.allCustomers();
+            ObservableList<String> list = FXCollections.observableArrayList();
+            for (CustomerDto dto: customers){
+                list.add(dto.getId());
+            }
+            cmbId.setItems(list);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void loadItemCodes() {
+        try {
+            items = itemModel.allItems();
+            ObservableList<String> list = FXCollections.observableArrayList();
+            for (ItemDto dto: items){
+                list.add(dto.getCode());
+            }
+            cmbCode.setItems(list);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     @FXML
     void backButtonOnAction(ActionEvent event) {
---
+++ src/main/java/model/ItemModel.java
@@ -0,0 +1,13 @@
+package model;
+
+import dto.ItemDto;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public interface ItemModel {
+    boolean saveItem(ItemDto itemDto);
+    boolean updateItem(ItemDto itemDto);
+    boolean deleteItem(String code);
+    List<ItemDto> allItems() throws SQLException, ClassNotFoundException;
+}
